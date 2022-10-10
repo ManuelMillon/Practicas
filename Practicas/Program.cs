bcb63b79c0a032c9e908a89e7fa4873471ba65a6
manolomillon <manolomillon@gmail.com>
2022-10-10 13:03:53 +0200

    Agregar archivos de proyecto.

---
+++ Practicas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //I define the variables
+
+            Console.WriteLine("Introduzca un valor para un radio: ");
+            int radio = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"El valor del radio es {radio}");
+            // System calculates the area
+            const double PI = 3.14;
+            double resultConversion = Convert.ToDouble(radio);
+            //Console.WriteLine($"El valor del radio convertido es {resultConversion}");
+            double area = PI * Math.Pow(resultConversion, 2);
+
+
+            //System 
+            // Console.WriteLine("El área del circulo de " + radio + " es " area);
+            Console.WriteLine($"El área del circulo de {radio} es {area}");
+        }
+    }
+}
